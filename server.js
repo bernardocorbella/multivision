7d4ec108e3098c3fed8cd9f17c754f9abbd40d76
Bernardo <bernardo@insignia4u.com>
2014-02-03 18:08:36 -0300

    Includes Bootstrap. Adds partial and content logic.

--- server.js
+++ server.js
@@ -12,14 +12,20 @@ function compile(str, path) {
 app.configure(function() {
     app.set('views', __dirname + '/server/views');
     app.set('view engine', 'jade');
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
     app.use(stylus.middleware(
         {
-            str: __dirname + '/public',
+            src: __dirname + '/public',
             compile: compile
         }
     ));
 
     app.use(express.static(__dirname + '/public'));
+});
+
+app.get('/partials/:partialPath', function(req, res) {
+    res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function(req, res) {
