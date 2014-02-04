1d954b040131301c511cb7e283fcfdb3c959cd9e
Bernardo <bernardo@insignia4u.com>
2014-02-04 09:20:17 -0300

    Moves partials to public/app.

---
+++ public/app/account/mvNavBarLoginCtrl.js
@@ -0,0 +1,6 @@
+angular.module('app').controller('mvNavBarLoginCtrl', function($scope) {
+    'use strict';
+    $scope.signin = function(username, password) {
+        console.log('needs revisit');
+    }
+});
