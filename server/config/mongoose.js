5822c5169183e2d76e8904a35b7393452878212f
Bernardo <bernardo@insignia4u.com>
2014-02-12 17:29:23 -0300

    First iteration at secure hashed passwords.
    Adds crypto for encription.
    Adds Salt and hash_pwd fields to userSchema.
    Generates dummy data with secure passwords.

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,25 +1,51 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    crypto = require('crypto');
 
 module.exports = function(config) {
-    "use strict";
-    mongoose.connect(config.db);
-    var db = mongoose.connection;
-    db.on('error', console.error.bind(console, "Connection Error..."));
-    db.once('open', function callback() {
-        console.log('MultiVision db connection opened');
-    });
+  "use strict";
+  mongoose.connect(config.db);
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, "Connection Error..."));
+  db.once('open', function callback() {
+      console.log('MultiVision db connection opened');
+  });
 
-    var userSchema = mongoose.Schema({
-      firstName: String,
-      lastName: String,
-      username: String,
-    });
-    var User = mongoose.model('User', userSchema);
+  var userSchema = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    username: String,
+    salt: String,
+    hashed_pwd: String,
+  });
+  userSchema.methods = {
+    authenticate: function(passwordToMatch) {
+      return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+    }
+  };
+  var User = mongoose.model('User', userSchema);
 
-    User.find({}).exec(function(err, collection) {
-      if (collection.length === 0) {
-        User.create({firstName: 'Admin', lastName: 'Of MultiVision', username: 'admin'});
-      };
-    });
+  User.find({}).exec(function(err, collection) {
+    if (collection.length === 0) {
+      var salt, hash;
+      salt = createSalt();
+      hash = hashPwd(salt, 'admin');
+      User.create({firstName: 'Admin', lastName: 'Of MultiVision', username: 'admin', salt: salt, hashed_pwd: hash});
+      salt = createSalt();
+      hash = hashPwd(salt, 'ber');
+      User.create({firstName: 'Bernardo', lastName: 'Corbella', username: 'ber', salt: salt, hashed_pwd: hash});
+      salt = createSalt();
+      hash = hashPwd(salt, 'john');
+      User.create({firstName: 'John', lastName: 'Doe', username: 'john', salt: salt, hashed_pwd: hash});
+    };
+  });
+}
 
+function createSalt() {
+  return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+  var hmac = crypto.createHmac('sha1', salt);
+
+  return hmac.update(pwd).digest('hex');
 }
